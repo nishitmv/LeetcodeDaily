1ba436617fff23ba3a1d9b1b8b1af1ba923f894a
nishit <nishitmv@gmail.com>
2022-07-11 02:15:56 +0530

    Remove Duplicates In Place allow two duplicates

---
+++ src/main/java/com/nitron/leetcodedaily/RemoveDuplicatesInPlace/DuplicatesRemoverII.java
@@ -0,0 +1,39 @@
+package com.nitron.leetcodedaily.RemoveDuplicatesInPlace;
+
+public class DuplicatesRemoverII {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        for(int i=0; i<n;i++)
+        {
+            int val = nums[i];
+            i++;
+            if(i<n && nums[i] == val)
+                i++;
+            while(i<n && nums[i] == val)
+            {
+                nums[i] = 1000;
+                i++;
+            }
+            i--;
+        }
+
+        int i=1, j=1;
+        while(i<n && j<n) {
+            if(nums[j] ==1000) {
+                while (j < n && nums[j] == 1000  ) {
+                    j++;
+                }
+                if(j<n) {
+                    nums[i] = nums[j];
+                    nums[j] = 1000;
+                    i++;
+                }
+            }
+            else {
+                i++;
+                j++;
+            }
+        }
+        return i;
+    }
+}
