b9254de0d15ff94eb14fad55860333400b369993
Nishit <nishit.vishwanath@tesco.com>
2022-07-20 16:03:13 +0530

    Rearrange string

---
+++ src/main/java/com/nitron/leetcodedaily/RearrangeString/Pair.java
@@ -0,0 +1,13 @@
+package com.nitron.leetcodedaily.RearrangeString;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Pair {
+
+    char key;
+    int frequency;
+
+}
