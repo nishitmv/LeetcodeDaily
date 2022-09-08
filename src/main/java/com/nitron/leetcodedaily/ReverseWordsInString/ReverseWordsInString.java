1de52dc7a8f17ebc28ab65e061073b9c39c856eb
nishit <nishitmv@gmail.com>
2022-09-08 23:33:30 +0530

    Reverse Words in a string

--- src/main/java/com/nitron/leetcodedaily/ReverseWordsInString/ReverseWordsInString.java
+++ src/main/java/com/nitron/leetcodedaily/ReverseWordsInString/ReverseWordsInString.java
@@ -3,31 +3,28 @@ package com.nitron.leetcodedaily.ReverseWordsInString;
 public class ReverseWordsInString {
 
     public static void main(String[] args) {
-        System.out.println(reverseWords("the sky is blue"));
+        System.out.println(reverseWords("the  sky  is blue"));
     }
 
     public static String reverseWords(String s) {
-    int spaceCount =0;
+
     StringBuilder builder = new StringBuilder();
     s = s.trim();
-        for( int i = s.length()-1; i>=0;i--)
+        int i = s.length()-1;
+        while( i>=0)
         {
-            if(s.charAt(i)==' ' )
+            int j=i+1;
+           while(i>=0 && s.charAt(i)!=' ')
+           {
+               i--;
+           }
+            builder.append(s, i+1, j).append(" ");
+            while(i>=0 && s.charAt(i)==' ')
             {
-                if( spaceCount==0)
-                {
-                builder.append(s.charAt(i));
-                    spaceCount++;
-            }
-            }
-            else {
-                builder.append(s.charAt(i));
-                spaceCount=0;
+                i--;
             }
         }
-
-
-        return builder.toString();
+        return builder.toString().trim();
     }
 
 }
